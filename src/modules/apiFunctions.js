3f16322b6b9c2ff1c71644a053c38dbf57eb6cdc
Jonash Gania <jonashtiglao@gmail.com>
2023-09-21 16:39:24 +0800

    fetching weather data

---
+++ src/modules/apiFunctions.js
@@ -0,0 +1,23 @@
+const apiKey = 'c359d505fd1ccc37e00fc497f93bccb4';
+
+async function fetchWeatherData(city){
+    try{
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${apiKey}`, {mode: 'cors'});
+    
+        if(!response.ok){
+            throw new Error('Network response was not ok');
+        }
+
+        const weatherData = await response.json();
+        console.log(weatherData);
+        return weatherData;
+
+    } catch(error){
+        console.log('Error', error);
+        throw error;
+    }
+}
+
+
+export { fetchWeatherData }
+
