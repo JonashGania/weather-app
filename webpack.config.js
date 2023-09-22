cdfa9d901d33bc89f7811e8eceb621c059855e0e
Jonash Gania <jonashtiglao@gmail.com>
2023-09-22 19:35:27 +0800

    changed output path

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,12 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: path.resolve(__dirname, 'src/index.js'),
   devServer: {
     static: './dist',
   },
   output: {
-    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
   },
 };
