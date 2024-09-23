cc5b7b2e3d08b1beade589f3818a269a380823b1
JonashGana <jonashtiglao@gmail.com>
2024-09-23 22:48:24 +0800

    initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,51 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { webpack, EnvironmentPlugin } = require("webpack");
 
 module.exports = {
-  mode: 'development',
-  entry: path.resolve(__dirname, 'src/index.js'),
-  devServer: {
-    static: './dist',
-  },
+  mode: "development",
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
-  devtool: 'inline-source-map'
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "assets/[name][ext]",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Todo list App",
+      filename: "index.html",
+      template: "./src/template.html",
+    }),
+    new EnvironmentPlugin({
+      API_KEY: "HJJW98N3NL4XVEPAVAVNCUWV7",
+    }),
+  ],
 };
