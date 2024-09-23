3feece62316aa9fd5218f8a02dd418880dba10f0
JonashGana <jonashtiglao@gmail.com>
2024-09-23 22:48:14 +0800

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,39 +1,18 @@
-import { fetchWeatherDataMetric } from "./modules/apiFunctions";
-import { displayWeatherDataMetric } from "./modules/dom";
+import "./styles/style.css";
+import { fetchCityWeatherData } from "./components/api";
+import DisplayWeatherData from "./components/RenderWeatherData";
+import DisplayHourlyForecast from "./components/RenderHourlyForecast";
+import DisplayDailyForecast from "./components/RenderDailyForecast";
 
+async function InitialLoad() {
+  const { weatherData, hourlyData, dailyData } =
+    await fetchCityWeatherData("Manila");
 
-const searchIcon = document.querySelector('.searchIcon');
-const searchInput = document.getElementById('search-location');
-
-async function initialLoad(){
-    const currentCity = await fetchWeatherDataMetric('Tokyo');
-    displayWeatherDataMetric(currentCity);
+  DisplayWeatherData(weatherData);
+  DisplayHourlyForecast(hourlyData);
+  DisplayDailyForecast(dailyData);
+  console.log(weatherData);
+  console.log(hourlyData);
 }
 
-async function handleSearch(){
-    const searchCity = searchInput.value.trim();
-    const errorMessage = document.querySelector('.error-message');
-
-
-    try{
-        const weatherData = await fetchWeatherDataMetric(searchCity);
-        displayWeatherDataMetric(weatherData);
-        searchInput.value = '';
-        errorMessage.textContent = '';
-
-    } catch (error){
-        errorMessage.textContent = `${error}. Check input location.`;
-    }
-}
-
-searchIcon.addEventListener('click', handleSearch);
-
-searchInput.addEventListener('keydown', (event) => {
-    if(event.key === 'Enter'){
-        event.preventDefault();
-        handleSearch();
-    }
-})
-
-
-initialLoad();
+InitialLoad();
