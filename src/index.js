246b8ae9d7b37ddda3b2789657468e57e4886e8a
JonashGana <jonashtiglao@gmail.com>
2024-09-25 21:36:21 +0800

    Code refactor

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import "./styles/style.css";
 import { fetchCityWeatherData } from "./components/api";
+import { EventHandlers } from "./components/eventHandlers";
+import { applySkeletonLoader } from "./components/utils";
 import DisplayWeatherData from "./components/RenderWeatherData";
 import DisplayHourlyForecast from "./components/RenderHourlyForecast";
 import DisplayDailyForecast from "./components/RenderDailyForecast";
 
 async function InitialLoad() {
+  applySkeletonLoader();
+
   const { weatherData, hourlyData, dailyData } =
-    await fetchCityWeatherData("Manila");
+    await fetchCityWeatherData("Tokyo");
 
   DisplayWeatherData(weatherData);
   DisplayHourlyForecast(hourlyData);
   DisplayDailyForecast(dailyData);
-  console.log(weatherData);
-  console.log(hourlyData);
+  EventHandlers();
 }
 
 InitialLoad();
