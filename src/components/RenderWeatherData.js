90f5430c7ec3545f9d1431009b3cb38e05e0c5c0
JonashGana <jonashtiglao@gmail.com>
2024-09-25 21:35:50 +0800

    Code refactor

--- src/components/RenderWeatherData.js
+++ src/components/RenderWeatherData.js
@@ -1,4 +1,8 @@
-import { checkWeatherIcon } from "./utils";
+import {
+  checkWeatherIcon,
+  capitalizeFirstLetters,
+  convertMilesToKm,
+} from "./utils";
 
 const DisplayWeatherData = (weatherData) => {
   const city = document.querySelector(".city");
@@ -10,13 +14,18 @@ const DisplayWeatherData = (weatherData) => {
   const weatherNowIcon = document.querySelector(".weather-now-condition-icon");
   const hourlyNowTemp = document.querySelector(".hourly-now-temp");
   const nowIcon = checkWeatherIcon(weatherData.currentConditions.icon);
+  weatherNowIcon.innerHTML = "";
 
-  city.textContent = weatherData.address;
+  city.classList.remove("skeleton");
+  temperature.classList.remove("skeleton");
+  weatherCondition.classList.remove("skeleton");
+
+  city.textContent = capitalizeFirstLetters(weatherData.address);
   temperature.textContent = `${weatherData.currentConditions.temp} °F`;
   weatherCondition.textContent = weatherData.currentConditions.conditions;
   feelslike.textContent = `${weatherData.currentConditions.feelslike} °F`;
   humidity.textContent = `${weatherData.currentConditions.humidity}%`;
-  windspeed.textContent = `${weatherData.currentConditions.windspeed}mph`;
+  windspeed.textContent = `${convertMilesToKm(weatherData.currentConditions.windspeed)}km/h`;
   hourlyNowTemp.textContent = `${weatherData.currentConditions.temp} °F`;
 
   weatherNowIcon.appendChild(nowIcon);
